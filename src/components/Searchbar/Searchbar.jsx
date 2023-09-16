2c2599dd03a0701a00f7d9f81cd2e5eef3ad9efc
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2023-09-16 11:09:31 +0300

    home-work

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { BtnLabel, SearchForm, SearchFormBtn, SearchInput, StyledSearchBar } from "./Searchbar.styled"
+
+export const Searchbar = ({onSubmit}) => {
+
+  const [query, setQuery] = React.useState('');
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (query.trim() === '') {
+      alert('Please enter a search query');
+      return;
+    }
+    onSubmit(query);
+  };
+
+  return (
+  <StyledSearchBar>
+   <SearchForm onSubmit={handleSubmit}>
+    <SearchFormBtn type="submit" name="query">
+      <BtnLabel>Search</BtnLabel>
+    </SearchFormBtn>
+
+    <SearchInput
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+          value={query}
+          onChange={handleInputChange}
+    />
+  </SearchForm>
+</StyledSearchBar>
+  );
+
+};
